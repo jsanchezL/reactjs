f2912aeaa5a85657fa71e10e9d35e11068803d2b
Jorge Luis Sánchez Labastida <jsanchez.labastida@gmail.com>
2020-06-03 16:50:28 -0500

    Leer un array desde un json

--- src/components/MiComponente.js
+++ src/components/MiComponente.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-class MiComponente extends React.Component{
+class MiComponente extends Component{
 
     render(){
+        let producto = {
+            name:'Pizza',
+            caracteristicas:[
+                '120gr',
+                '60Kcal',
+                '50MXN'
+            ]
+        };
+
         return (
-            <h2>Hola, soy el componente llamado: MiComponente</h2>
+            <React.Fragment>
+                <h1>{'Nombre: ' + producto.name}</h1>
+                <h2>Caracteristicas</h2>
+                <ol>                    
+                   {
+                      producto.caracteristicas.map((car,i) => {
+                      return <li key={i}>{car}</li>
+                      }) 
+                   }
+                </ol>
+                <hr/>
+            </React.Fragment>
         );
     }
 }
