d966e458b2876259fb97cdd6e7074c1f2d6dffc4
Jorge Luis Sánchez Labastida <jsanchez.labastida@gmail.com>
2020-06-03 18:15:57 -0500

    Con datos de prueba para mostrar en pantalla simulando un objeto real para un componente en especifico.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react'
+import './assets/css/App.css';
+import tareas from './samples/tareas.json';
 
 class App extends Component{
+  state = {
+    tareas: tareas
+  }
+
   render() {
-    return <div>Hi World!</div>
+    return (
+    <div>
+      { this.state.tareas.map(e => <p key={e.id}>{e.name} - {e.description} - {e.state} </p>)}
+    </div>
+    )
   }
 }
 
