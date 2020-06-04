eb6365b35392f267b3b1c717cc0840275543db98
Jorge Luis Sánchez Labastida <jsanchez.labastida@gmail.com>
2020-06-04 11:16:11 -0500

    Aplicando estilos en linea a un componente

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,11 +4,18 @@ import React, { Component } from 'react';
  * Para renderear un componente para una sola tarea
  */
 class Task extends Component{
+
+    setCompleteTask(tarea) {
+        return {
+            color: (tarea.state === "Done") ? 'gray' : 'black',
+            textDecoration: (tarea.state === "Done") ? 'line-through' : 'none'
+        }
+    }
     
     render() {
         const {tarea} = this.props;
         return (
-            <div>           
+            <div style={this.setCompleteTask(tarea)}>           
                 {tarea.name} - {tarea.description} - {tarea.state} 
                 <input type="checkbox"/>
                 <button>X</button>
