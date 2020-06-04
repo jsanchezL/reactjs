885daf74c5969fd4c1c8af01095a434250e3de0a
Jorge Luis Sánchez Labastida <jsanchez.labastida@gmail.com>
2020-06-04 12:25:38 -0500

    Forms en ReacJS

---
+++ src/components/TaskForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+export default class TaskForm extends Component {
+    state = {
+        name : '',
+        description : ''
+    }
+    
+    // Omitir metodo bind
+    onSubmit = e => {
+        console.log(this.state);
+        e.preventDefault(); //Prevenir que la pagina se refresque cada que da click al guardar
+    }
+
+    onChange = e => {
+        console.log(e.target.name);
+        console.log(e.target.value);
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.onSubmit}>
+                <input type="text" name="name" placeholder="Titulo de la tarea" onChange={this.onChange} value={this.state.name} />
+                <br/>
+                <br/>
+                <textarea name="description" placeholder="Detalles de la tarea" onChange={this.onChange} value={this.state.description} />
+                <button type="submit">
+                    Guardar
+                </button>
+            </form>
+        )
+    }
+}
